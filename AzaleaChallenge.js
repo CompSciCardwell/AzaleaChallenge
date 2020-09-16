e03b7a8dab392591b66e9661098643ccf6706f5e
Ben Cardwell <compscicardwell@gmail.com>
2020-09-16 14:33:34 -0400

    Update AzaleaChallenge.js

--- AzaleaChallenge.js
+++ AzaleaChallenge.js
@@ -1,6 +1,6 @@
 function lowestOfThree(array1, array2, array3) {
 
-    for (var i = 0; i < array1.length; i++){
+    for (var i = 0; i < array1.length; i++) {
         var myNum = array1[i];
         if (array2.indexOf(myNum) != -1) {
             if (array3.indexOf(myNum) != -1) {
