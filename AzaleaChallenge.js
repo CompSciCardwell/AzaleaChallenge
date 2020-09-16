ab471d6ac2fdb15e4ce023ff6a066fd0e8469190
Ben Cardwell <compscicardwell@gmail.com>
2020-09-16 14:14:59 -0400

    Add files via upload
    First commit

---
+++ AzaleaChallenge.js
@@ -0,0 +1,14 @@
+function lowestOfThree(array1, array2, array3) {
+
+    for (var i = 0; i < array1.length; i++){
+        var myNum = array1[i];
+        if (array2.indexOf(myNum) != -1)
+        {
+            if (array3.indexOf(myNum) != -1)
+            {
+                return array1[i];
+            }
+        }
+    }
+    return false;
+}
